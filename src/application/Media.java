a8f99e04f5746fa295359a24f7984db9cba87e19
WillianGilloni <willianggvet@gmail.com>
2022-03-05 13:37:29 -0300

    Calculando media do aluno

---
+++ src/application/Media.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.PassOuFailed;
+
+public class Media {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		 PassOuFailed student = new PassOuFailed();
+		
+		student.name = sc.nextLine();
+		student.grade1 = sc.nextDouble();
+		student.grade2 = sc.nextDouble();
+		student.grade3 = sc.nextDouble();
+		
+		System.out.printf("Final GRADE: %.2f%n", student.finalGrade());
+		
+		System.out.printf("FINAL GRADE: %.2f%n", student.finalGrade());
+		
+		if (student.finalGrade() < 60.0) {
+		System.out.println("FAILED");
+		System.out.printf("MISSING %.2f POINTS%n", student.missingPoints());
+		}
+		else {
+		System.out.println("PASS");
+		}
+		sc.close();
+	}
+
+}
